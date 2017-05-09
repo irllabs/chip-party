7aef36b5f50da1b628aa8b9870190015b1120ca2
Jesse Stiles <jessestiles@gmail.com>
2017-05-09 12:48:21 -0400

    fixed sizeof things to calculate elements, etc

--- Teensy-MIDI/Arduino Sketch/teensy3.2_midi_IO_04/teensy3.2_midi_IO_04.ino
+++ Teensy-MIDI/Arduino Sketch/teensy3.2_midi_IO_04/teensy3.2_midi_IO_04.ino
@@ -4,28 +4,28 @@
 // the MIDI channel number to send messages
 const int channel = 1;
 
-const int ledArray[] = {5, 6, 20, 21}; //led pin numbers
+const int ledPins[] = {5, 6, 20, 21}; //led pin numbers
 const int ledNotes[] = {60, 61, 62, 63}; //note numbers for leds
-const int leds = sizeof(ledArray); //used for loops
+const int leds = sizeof(ledPins)/sizeof(leds); //used for loops
 
-const int faderArray[] = {A0, A9, A20, A10, A11, A12}; //fader pins
+const int faderPins[] = {A0, A9, A20, A10, A11, A12}; //fader pins
 const int faderCtls[] = {14, 15, 16, 17, 18, 19}; //fader MIDI CC #s
-const int faders = sizeof(faderArray); //used for loops
+const int faders = sizeof(faderPins)/sizeof(faders); //used for loops
 int faderPrevs[faders]; //used to store previous values
 
-const int joyPotArray[] = {A15, A16, A18, A17}; //pins
+const int joyPotPins[] = {A15, A16, A18, A17}; //pins
 const int joyPotCtls[] = {20, 21, 22, 23}; //CCs
-const int joyPots = sizeof(joyPotArray); //for loops
+const int joyPots = sizeof(joyPotPins)/sizeof(joyPots); //for loops
 int joyPotPrevs[joyPots]; //previous values
 
-const int joyButtonArray[] = {24, 30}; //pins
+const int joyButtonPins[] = {24, 30}; //pins
 const int joyButtonNotes [] = {65, 66}; //note numbers
-const int joyButtons = sizeof(joyButtonArray); // for loops
+const int joyButtons = sizeof(joyButtonPins)/sizeof(joyButtons); // for loops
 Bounce * joyButtDebouncer = new Bounce[joyButtons]; // Create Bounce objects for each button.
 
-const int buttonArray[] = {13, 14}; //pins
+const int buttonPins[] = {13, 14}; //pins
 const int buttonNotes[] = {61, 62}; //note nums
-const int buttons = sizeof(buttonArray); //for loops
+const int buttons = sizeof(buttonPins)/sizeof(buttons); //for loops
 Bounce * buttDebouncer = new Bounce[buttons]; // Create Bounce objects for each button.
 
 elapsedMillis msec = 0;
@@ -42,15 +42,15 @@ void setup() {
   } // set all previous joystick potentiometer values to -1 at startup
 
   for (int b = 0; b < joyButtons; b++) {
-    pinMode(joyButtonArray[b], INPUT_PULLUP);
-    joyButtDebouncer[b].attach(joyButtonArray[b]);
+    pinMode(joyButtonPins[b], INPUT_PULLUP);
+    joyButtDebouncer[b].attach(joyButtonPins[b]);
     joyButtDebouncer[b].interval(5);
   } //set up button pins and attach them to Bounce objects and set time interals
 
 
   for (int b = 0; b < buttons; b++) {
-    pinMode(buttonArray[b], INPUT_PULLUP);
-    buttDebouncer[b].attach(buttonArray[b]);
+    pinMode(buttonPins[b], INPUT_PULLUP);
+    buttDebouncer[b].attach(buttonPins[b]);
     buttDebouncer[b].interval(5);
   } //set up button pins and attach them to Bounce objects and set time interals
 
@@ -89,14 +89,14 @@ void faderInput(){
   if (msec >= 20) {
     msec = 0;
     for (int b = 0; b < faders; b++) {
-      int f = analogRead(faderArray[b]) / 8;
+      int f = analogRead(faderPins[b]) / 8;
       if (f != faderPrevs[b]) {
         usbMIDI.sendControlChange(faderCtls[b], f, channel);
         faderPrevs[b] = f;
       }
     }
     for (int b = 0; b < joyPots; b++) {
-      int f = analogRead(joyPotArray[b]) / 8;
+      int f = analogRead(joyPotPins[b]) / 8;
       if (f != joyPotPrevs[b]) {
         usbMIDI.sendControlChange(joyPotCtls[b], f, channel);
         joyPotPrevs[b] = f;
@@ -110,7 +110,7 @@ void OnNoteOn(byte channel, byte note, byte velocity)
 
   for (int b = 0; b < leds; b++) {
   if (note==ledNotes[b]){
-    analogWrite(ledArray[b], velocity*2);
+    analogWrite(ledPins[b], velocity*2);
   }
   }
     
@@ -121,7 +121,7 @@ void OnNoteOff(byte channel, byte note, byte velocity)
 
   for (int b = 0; b < leds; b++) {
   if (note==ledNotes[b]){
-    analogWrite(ledArray[b], 0);
+    analogWrite(ledPins[b], 0);
   }
   }
     
